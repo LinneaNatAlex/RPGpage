389ff4bb6a237eb8894c22acb510fdd3272e2e85
LinneaNatAlex <linnea.n.a.s.hetty@outlook.com>
2025-05-15 10:53:13 +0200

    added Sign up button, and sign in button on the main Page

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -2,6 +2,7 @@ import styles from './MainPage.module.css';
 import { useEffect, useState } from 'react';
 import { auth } from '../../firebaseConfig';
 import { onAuthStateChanged } from 'firebase/auth';
+import { Link } from 'react-router-dom';
 
 
 
@@ -14,7 +15,7 @@ useEffect(() => {
         if (currentUser) {
             setUser(currentUser.displayName || currentUser.email);
         } else {
-            setUser('Guest');
+            setUser(null);
         }
     });
     return () => unsubscribe();
@@ -25,6 +26,14 @@ useEffect(() => {
         <header className={styles.introductionHeader}>
             <h1 className={styles.introductionTitle}> Welcome {user || 'Guest'} </h1>
             <p className={styles.introductionText}>Welcome to the world of magic and wonder! Join us on an enchanting journey where you can explore the mysteries of the universe, learn powerful spells, and discover the secrets of the magical realm. Whether you're a seasoned wizard or a curious newcomer, there's something for everyone in this magical adventure. So grab your wand and get ready to embark on an unforgettable quest!</p>
+       
+        {/* to make sure the only see the links if they are not logged in */}
+            {!user && (
+                <>
+                    <Link to='/sign-in' className={styles.ctaBtn}>Enter the Castle</Link>
+                    <Link to='/sign-up' className={styles.ctaBtn}>Create an Account</Link>
+                </>
+            )}
         </header>
     </section>
 };
