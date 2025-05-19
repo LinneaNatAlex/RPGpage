be0278049a07bd093413623704c5d4c472a9a151
LinneaNatAlex <linnea.n.a.s.hetty@outlook.com>
2025-05-19 03:45:33 +0200

    Lots of changes

---
+++ src/Components/OnlineUsers/OnlineUsers.jsx
@@ -0,0 +1,23 @@
+import style from "./OnlineUsers.module.css";
+import useOnlineUsers from "../../hooks/useOnlineUsers";
+
+const OnlineUsers = () => {
+    const users = useOnlineUsers();
+
+    if (!users.length) return null;
+// this is where users 'online' are displayed
+
+    return (
+        <div className ={style.onlineUsersContainer}>
+            <h2>Is online Witch/Wizard</h2>
+            <ul className={style.onlineUsersList}>
+                {users.map((user) => (
+                    <li key={user.id} className={style.onlineUserItem}>
+                        {user.displayName}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+export default OnlineUsers;
