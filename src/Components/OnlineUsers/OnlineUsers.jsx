d17d32ac8fc866cde477755f404f76424999a3ef
LinneaNatAlex <linnea.n.a.s.hetty@outlook.com>
2025-05-26 18:02:32 +0200

    Searchbar uppdate fixed

--- src/Components/OnlineUsers/OnlineUsers.jsx
+++ src/Components/OnlineUsers/OnlineUsers.jsx
@@ -2,22 +2,22 @@ import style from "./OnlineUsers.module.css";
 import useOnlineUsers from "../../hooks/useOnlineUsers";
 
 const OnlineUsers = () => {
-    const users = useOnlineUsers();
+  const users = useOnlineUsers();
 
-    if (!users.length) return null;
-// this is where users 'online' are displayed
+  if (!users.length) return null;
+  // this is where users 'online' are displayed
 
-    return (
-        <div className ={style.onlineUsersContainer}>
-            <h2>Is online Witch/Wizard</h2>
-            <ul className={style.onlineUsersList}>
-                {users.map((user) => (
-                    <li key={user.id} className={style.onlineUserItem}>
-                        {user.displayName}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className={style.onlineUsersContainer}>
+      <h2>Is online Witch/Wizard</h2>
+      <ul className={style.onlineUsersList}>
+        {users.map((user) => (
+          <li key={user.id} className={style.onlineUserItem}>
+            {user.displayName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default OnlineUsers;
--- src/Pages/UserMap/UserMap.jsx
+++ src/Pages/UserMap/UserMap.jsx
@@ -2,15 +2,23 @@ import style from "./UserMap.module.css";
 import { useState } from "react";
 import UserList from "../../Components/UserList/UserList.jsx";
 import styles from "../../Components/UserList/UserList.module.css";
+import SearchBar from "../../Components/SearchBar/SearchBar.jsx";
 
 // This is the UserMap page where users will be displayed
 const UserMap = () => {
   const [userQuery, setUserQuery] = useState("");
 
   return (
-    <div className={style.userMapContainer}>
-      <UserList userQuery={userQuery} />
-    </div>
+    <>
+      {/* SearchBar that connect to the user list */}
+      <div className={style.userSearchBar}>
+        <SearchBar setUserQuery={setUserQuery} userQuery={userQuery} />
+      </div>
+      {/* Userlist that displays either full list or filter list */}
+      <div className={style.userMapContainer}>
+        <UserList userQuery={userQuery} />
+      </div>
+    </>
   );
 };
 
