8b143414c3cb067c68ff5f3981d1e48d9c269f3b
LinneaNatAlex <linnea.n.a.s.hetty@outlook.com>
2025-05-27 18:07:35 +0200

    fixed bug in the email berification system

--- src/hooks/useNewsFeed.js
+++ src/hooks/useNewsFeed.js
@@ -3,33 +3,30 @@ import { db } from "../firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
 
 const useNewsFeed = () => {
-  const [news, setNews] = useState([]);
-  const [titles, setTitle] = useState([]);
+  const [news, setNews] = useState([]); // saves the news data in the state variable
 
+  // fetches the documnet from the NEWS collectins, and stores all the data in the news state variable
   const fetchNews = async () => {
     try {
+      // This fetched all of the documents form the news collection in the firestore db.
       const snapshot = await getDocs(collection(db, "news"));
       const newsData = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
-
-      const titlesOnly = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        title: doc.data().title,
-      }));
-      setTitle(titlesOnly);
       setNews(newsData);
     } catch (error) {
       console.error("Error fetching news: ", error);
     }
   };
-
+  //  USE EFFECT HOOK uset to gather the news data when the component is mounted
   useEffect(() => {
     fetchNews();
   }, []);
 
-  return { news, titles, refetchNews: fetchNews };
+  // Returns data and the to again gather the news data.
+
+  return { news, refetchNews: fetchNews };
 };
 
 export default useNewsFeed;
