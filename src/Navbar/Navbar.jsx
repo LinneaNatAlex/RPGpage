0733ad09e548db3137af4057ccb2ceae57da2800
LinneaNatAlex <linnea.n.a.s.hetty@outlook.com>
2025-05-29 16:32:36 +0200

    fixed responsive signin and signup

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -40,22 +40,24 @@ const Navbar = () => {
     <nav className={styles.navbar}>
       {isLoggedIn ? (
         <>
-          <NavLink to="/">Hogwart Castel</NavLink>
-          <NavLink to="/Profile">Profile</NavLink>
-          <NavLink to="/userMap">User Map</NavLink>
+          <div className={styles.menuItems}>
+            <NavLink to="/">Hogwart Castel</NavLink>
+            <NavLink to="/Profile">Profile</NavLink>
+            <NavLink to="/userMap">User Map</NavLink>
 
-          <div className={styles.classroomsDropdown}>
-            <NavLink to="/ClassRooms">Class Rooms</NavLink>
-            <div className={styles.dropdown}>
-              <NavLink to="/ClassRooms/Potions">Potions</NavLink>
+            <div className={styles.classroomsDropdown}>
+              <NavLink to="/ClassRooms">Class Rooms</NavLink>
+              <div className={styles.dropdown}>
+                <NavLink to="/ClassRooms/Potions">Potions</NavLink>
+              </div>
             </div>
+
+            {/* Makes the butten only avalible when logged in */}
+
+            <button onClick={handleSignOut} className={styles.signOutBtn}>
+              Leave Castle
+            </button>
           </div>
-
-          {/* Makes the butten only avalible when logged in */}
-
-          <button onClick={handleSignOut} className={styles.signOutBtn}>
-            Leave Castle
-          </button>
         </>
       ) : (
         <>
