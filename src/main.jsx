f1e7c706c0f2414eb138c0612d7dbb30ede77bff
LinneaNatAlex <linnea.n.a.s.hetty@outlook.com>
2025-06-04 18:03:44 +0200

    resolved issues

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "./context/authContext.jsx";
 
 // root element for the application
 createRoot(document.getElementById("root")).render(
