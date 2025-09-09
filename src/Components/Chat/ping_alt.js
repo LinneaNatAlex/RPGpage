387fb511c15b21a16db95bdbb1273ee492f77cfb
LinneaNatAlex <linnea.n.a.s.hetty@outlook.com>
2025-09-09 15:49:29 +0200

    Fjern alert-popup ved lydfeil i playPing

--- src/Components/Chat/ping_alt.js
+++ src/Components/Chat/ping_alt.js
@@ -4,10 +4,5 @@ export const playPing = () => {
     "https://actions.google.com/sounds/v1/alarms/beep_short.ogg" // Google public beep
   );
   audio.volume = 1.0;
-  audio.play().catch((e) => {
-    // Fanger feil hvis nettleseren blokkerer lyd
-    alert(
-      "Kunne ikke spille av lyd. Prøv å klikke et annet sted på siden først!"
-    );
-  });
+  audio.play().catch(() => {});
 };
